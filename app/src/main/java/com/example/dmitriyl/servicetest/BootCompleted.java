28cb235c66a6b6a57e8e5874187ccf54de337f3a
dmitriy.l <dimalee70>
2018-08-13 14:00:36 +0600

    Add sevice whish notifying gps location

--- app/src/main/java/com/example/dmitriyl/servicetest/BootCompleted.java
+++ app/src/main/java/com/example/dmitriyl/servicetest/BootCompleted.java
@@ -1,11 +1,22 @@
 package com.example.dmitriyl.servicetest;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+
+import static android.content.Context.LOCATION_SERVICE;
 
 public class BootCompleted extends BroadcastReceiver
 {
+    Location location;
+
+    LocationManager locationManager;
     @Override
     public void onReceive(Context context, Intent intent)
     {
@@ -13,6 +24,32 @@ public class BootCompleted extends BroadcastReceiver
 //        {
             Intent serviceIntent = new Intent(context,AndroidStartServiceOnBoot.class);
             context.startService(serviceIntent);
+//            locationManager = (LocationManager)context.getSystemService(LOCATION_SERVICE);
+//            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                System.out.println("inside check permission");
+//                ActivityCompat.requestPermissions((Activity) context,   new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+//            }
+//            System.out.println(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude());
 //        }
     }
+
+//    public Location getLocation(Context context) {
+//        try {
+//            System.out.println("Start get loc");
+//            locationManager = (LocationManager)context.getSystemService(LOCATION_SERVICE);
+//            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                System.out.println("inside check permission");
+//                ActivityCompat.requestPermissions((Activity) context,   new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+//            }
+//            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//            System.out.println(location.getLatitude());
+////                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+////            System.out.println("Latitude " +locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude());
+////            System.out.println("Longitude " + locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude());
+//        }catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//        return location;
+//    }
 }
--- app/src/main/java/com/example/dmitriyl/servicetest/MainActivity.java
+++ app/src/main/java/com/example/dmitriyl/servicetest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.dmitriyl.servicetest;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.ComponentName;
@@ -9,9 +11,16 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
+    private PendingIntent pendingIntent;
+    private AlarmManager manager;
 
+    @SuppressLint("ShortAlarm")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +29,34 @@ public class MainActivity extends Activity {
         AlarmManager alarmManager=(AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this,BootCompleted.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this,0,intent,0);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),1,pendingIntent);
+        assert alarmManager != null;
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),10,pendingIntent);
+//
+//
+//
+//       ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+//        List<ActivityManager.RunningServiceInfo> rs = activityManager.getRunningServices(50);
+//
+//        for (int i = 0; i < rs.size(); i++) {
+//            ActivityManager.RunningServiceInfo rsi = rs.get(i);
+//            System.out.println("Service" + "," + "Process " + rsi.process + " with component "
+//                    + rsi.service.getClassName());
+//        }
+
+
+
+
+//        Intent intent = new Intent(this,AndroidStartServiceOnBoot.class);
+//        startService(intent);
+//        this.sendBroadcast(intent);
+
+        //true
 
 //        BootCompleted bootCompleted = new BootCompleted();
 //        Intent intent = new Intent(this,AndroidStartServiceOnBoot.class);
 //        bootCompleted.onReceive(this,intent);
 
-        //        ComponentName componentName =  new ComponentName(this, BootCompleted.class);
+//        ComponentName componentName =  new ComponentName(this, BootCompleted.class);
 //        PackageManager packageManager = getPackageManager();
 //        packageManager.setComponentEnabledSetting(componentName,
 //                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
@@ -35,4 +65,5 @@ public class MainActivity extends Activity {
 //        Intent serviceIntent = new Intent(this,AndroidStartServiceOnBoot.class);
 //        startService(serviceIntent);
     }
+//
 }
